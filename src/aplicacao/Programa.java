ea9dba5dc4c811c5bc78bc445aad0a98e29f7a9a
Hebert Brito <hbdev1211@gmail.com>
2023-06-29 21:08:53 -0300

    Department entity class

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,17 @@
+package aplicacao;
+
+import model.entities.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Departamento depart = new Departamento(1, "Books");
+		
+		System.out.println(depart);
+		
+		
+		
+	}
+
+}

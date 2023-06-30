9feb783807eb7fa1d6e9b9ec8ae9cb1b9f03e549
Hebert Brito <hbdev1211@gmail.com>
2023-06-29 21:56:57 -0300

    VendedorJDBC e FabricaDAO

---
+++ src/model/dao/implementacao/VendedorDaoJDBC.java
@@ -0,0 +1,39 @@
+package model.dao.implementacao;
+
+import java.util.List;
+
+import model.dao.VendedorDAO;
+import model.entities.Vendedor;
+
+public class VendedorDaoJDBC implements VendedorDAO {
+
+	@Override
+	public void inserir(Vendedor Vendedor) {
+		
+		
+	}
+
+	@Override
+	public void atualizar(Vendedor Vendedor) {
+		
+	}
+
+	@Override
+	public void deletarPorId(Integer id) {
+		
+		
+	}
+
+	@Override
+	public void encontrarPorId(Integer id) {
+		
+		
+	}
+
+	@Override
+	public List<Vendedor> encontrarTodos() {
+	
+		return null;
+	}
+
+}
